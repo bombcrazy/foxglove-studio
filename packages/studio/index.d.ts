64651cd561b7a3e6de5958c6de344dff36560f1f
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-01-25 13:50:17 -0800

    whoops, how clumsy of me

--- packages/studio/index.d.ts
+++ packages/studio/index.d.ts
@@ -2,6 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+export type Foo = Foo;
 declare module "@foxglove/studio" {
   // Valid types for parameter data (such as rosparams)
   export type ParameterValue =
